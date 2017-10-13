77423b1cce7c085c542f8eaa362a1b138df4dbb1
Oh Hyun Seok <ohsai@snu.ac.kr>
2017-10-13 10:33:26 +0900

    preprocessreview  interface matching
    gogo

--- wordvectormaker.py
+++ wordvectormaker.py
@@ -5,7 +5,37 @@ word2vec_model_path = './models/namuwiki_testmodel'
 word2vec_model = gensim.models.Word2Vec.load(word2vec_model_path)
 
 
-class PreprocessedReview(object):
+class PreprocessReview(object):
+    def __init__(self, company_postag, company_word2vec, context_postag, context_word2vec,
+                 id, rate, context, post_time, label, review_id): # argument 'context' why does it exist?
+        self.company_postag = company_postag
+        self.company_word2vec = company_word2vec
+        self.context_postag = context_postag
+        self.context_word2vec = context_word2vec
+
+        self.id = id
+        self.rate = rate
+        self.post_time = post_time
+        self.label = label
+        self.review_id = review_id
+
+        self.db_node = None
+
+    def __str__(self):
+        return ("PreprocessReview object {6} :\n"
+                "  Company name _postagged = {0}\n"
+                "  ID = {1}\n"
+                "  Rating = {2}\n"
+                "  Context _postagged = {3}\n"
+                "  Post time = {4}\n"
+                "  Spam/Ham = {5}\n"
+                "  Company name _vectorized = {7}\n"
+                "  Context _vectorized = {8}\n"
+                .format(self.company_postag, self.id, self.rate,
+                    self.context_postag, self.post_time, self.label,
+                    self.review_id,self.company_word2vec,self.context_word2vec))
+    """
+class PreprocessReview(object):
     def __init__(self, word2vec_model_input, BookingReview_input):
 
         self.company_postag = BookingReview_input.company # string list
@@ -36,34 +66,70 @@ class PreprocessedReview(object):
 
         self.db_node = BookingReview_input.db_node
 
-
+"""
 class BookingReview(object):
-
-    def __init__(self, company, id, rate, context, post_time, spam_ham,review_id,db_node):
-
+    def __init__(self, company, id, rate, context, post_time, label,
+                 review_id):
         self.company = company
-
         self.id = id
-
         self.rate = rate
-
         self.context = context
-
         self.post_time = post_time
-
-        self.spam_ham = spam_ham
+        self.label = label
         self.review_id = review_id
-        self.db_node = db_node
 
-def BookingReview_list_to_PreprocessedReview_list(word2vec_model_input,BookingReview_list):
+        self.db_node = None
+
+    def __str__(self):
+        return ("BookingReview object {6}:\n"
+                "  Company name = {0}\n"
+                "  ID = {1}\n"
+                "  Rating = {2}\n"
+                "  Current Context = {3}\n"
+                "  Post time = {4}\n"
+                "  Spam/Ham = {5}\n"
+                .format(self.company, self.id, self.rate,
+                        self.context, self.post_time, self.label,
+                        self.review_id))
+
+    def __lt__(self, cmp):
+        if (self.id > cmp.id):
+            return False
+        elif (self.id < cmp.id):
+            return True
+        elif (self.post_time > cmp.post_time):
+            return False
+        else:
+            return True
+
+def BookingReview_to_PreprocessReview(word2vec_model_input, BookingReview_input):
+
+    # Company word2vec
+    company_word2vec_output = []
+    wordlist_iterator = iter(BookingReview_input.company)
+    for word in wordlist_iterator:
+        company_word2vec_output.append(word2vec_model_input.wv[word])  # wv is dictionary and word is string.
+    company_word2vec_output = np.array(company_word2vec_output)
+
+    # Context word2vec
+    context_word2vec_output = []
+    wordlist_iterator = iter(BookingReview_input.context)
+    for word in wordlist_iterator:
+        context_word2vec_output.append(word2vec_model_input.wv[word])  # wv is dictionary and word is string.
+    context_word2vec_output = np.array(context_word2vec_output)
+
+    return PreprocessReview(BookingReview_input.company,company_word2vec_output,BookingReview_input.context,context_word2vec_output,
+                            BookingReview_input.id,BookingReview_input.rate,BookingReview_input.context,
+                            BookingReview_input.post_time,BookingReview_input.label,BookingReview_input.review_id)
+
+def BookingReview_list_to_PreprocessReview_list(word2vec_model_input,BookingReview_list):
     output = []
     list_iterator = iter(BookingReview_list)
     for item_BookingReview in list_iterator:
-        output.append(PreprocessedReview(word2vec_model_input,item_BookingReview))
+        output.append(BookingReview_to_PreprocessReview(word2vec_model_input,item_BookingReview))
     return output
-"""
-test = [BookingReview(['여담/Noun','으로/Josa'],'paradox',10,['세계/Noun', '수의/Noun', '미궁/Noun' ,'시리즈/Noun'],10,True,'Zeus','faker')]
+
+test = [BookingReview(['여담/Noun','으로/Josa'],'paradox',10,['세계/Noun', '수의/Noun', '미궁/Noun' ,'시리즈/Noun'],10,True,'Zeus')]
 #test2 = PreprocessedReview(word2vec_model,test)
-test3 = BookingReview_list_to_PreprocessedReview_list(word2vec_model,test)
-print(test3[0].company_word2vec)
-"""
+test3 = BookingReview_list_to_PreprocessReview_list(word2vec_model,test)
+print(test3[0])
