4ab70fb05f5b4e957f213e32ed591960f456aaa6
PPPP <illhyhl1111@snu.ac.kr>
2017-10-13 12:35:24 +0900

    merging

--- Preprocessor/wordvectormaker.py
+++ Preprocessor/wordvectormaker.py
@@ -1,35 +1,6 @@
 import gensim
 import numpy as np
-
-class PreprocessReview(object):
-    def __init__(self, company_postag, company_word2vec, context_postag, context_word2vec,
-                 id, rate, post_time, label, review_id): # argument 'context' why does it exist?
-        self.company_postag = company_postag
-        self.company_word2vec = company_word2vec
-        self.context_postag = context_postag
-        self.context_word2vec = context_word2vec
-
-        self.id = id
-        self.rate = rate
-        self.post_time = post_time
-        self.label = label
-        self.review_id = review_id
-
-        self.db_node = None
-
-    def __str__(self):
-        return ("PreprocessReview object {6} :\n"
-                "  Company name _postagged = {0}\n"
-                "  ID = {1}\n"
-                "  Rating = {2}\n"
-                "  Context _postagged = {3}\n"
-                "  Post time = {4}\n"
-                "  Spam/Ham = {5}\n"
-                "  Company name _vectorized = {7}\n"
-                "  Context _vectorized = {8}\n"
-                .format(self.company_postag, self.id, self.rate,
-                    self.context_postag, self.post_time, self.label,
-                    self.review_id,self.company_word2vec,self.context_word2vec)) # [0][0] for observation.. later need revision
+import preprocessreview
 
 def word2vec_to_PreprocessReview(word2vec_model_input, PreprocessReview_input):
 
